ea9bb79c8c75601b6d564069ffb94319009df59f
AJ Nieminen <ajnieminen@kapsi.fi>
2020-11-22 20:11:31 +0200

    changes

--- telegram_bot.py
+++ telegram_bot.py
@@ -29,6 +29,7 @@ import json
 import nacl.secret
 import nacl.utils
 
+import arrow
 
 from os import environ
 
@@ -37,24 +38,25 @@ from os import environ
 
 db = SqliteDatabase('spc.db')
 
+ROLL_PRICE = 100
+
+
 class Roll(Model):
-    user_id = IntegerField()
+    user_id = IntegerField(primary_key=True)
     count = IntegerField()
 
     class Meta:
         database = db
 
 class Transaction(Model):
-    to_address = CharField()
-    from_address = CharField()
-    amount = IntegerField()
+    address = CharField(primary_key=True)
     timestamp = DateTimeField()
 
     class Meta:
         database = db
 
 class Address(Model):
-    user_id = IntegerField()
+    user_id = IntegerField(primary_key=True)
     username = CharField()
     address = CharField()
 
@@ -72,6 +74,23 @@ def _create_tables():
 def _get_keys(secret_key):
     public_key = crypto_sign_ed25519_sk_to_pk(bytes.fromhex(secret_key))
     return secret_key, public_key.hex()
+
+def _get_roll_count(user_id):
+    try:
+        return Roll.get(Roll.user_id==user_id).count
+    except Exception as e:
+        return 0
+
+def _set_roll_count(user_id, count):
+    try:
+        Roll\
+            .insert(user_id=user_id, count=count)\
+            .on_conflict('replace')\
+            .execute()
+        return True
+    except Exception as e:
+        print('e', e)
+        return False
 
 _create_tables()
 token = environ['tbot_api']
@@ -103,26 +122,87 @@ def address(update: Update, context: CallbackContext) -> None:
                        address=arguments[1])
         update.message.reply_text('Osoite lisätty!')
 
-def message(update, context):
-    print('update')
 
+def roll(update: Update, context: CallbackContext) -> None:
+    arguments = update.message.text.split(' ')
+    try:
+        user_id = update.effective_user.id
+        address = Address.get(Address.user_id==user_id)
+        roll_count = _get_roll_count(user_id)
+        update.message.reply_text(f'Pelejä jäljellä: {roll_count}')
+    except Exception as e:
+        update.message.reply_text('Osoitetta ei lisätty.')
+
+def message(update, context):
+    try:
+        user_id = update.effective_user.id
+        if '🎰' == update.message.dice.emoji:
+            roll_count = _get_roll_count(user_id)
+            if roll_count > 0:
+                _set_roll_count(user_id, roll_count - 1)
+                print('rolled!')
+            else:
+                print('no rolls')
+            print('update', update.message.dice)
+    except:
+        pass
+
+
+updater.dispatcher.add_handler(MessageHandler(filters=Filters.all,
+                                              callback=message))
 updater.dispatcher.add_handler(CommandHandler(command='osoite',
                                               filters=Filters.all,
                                               callback=address))
+updater.dispatcher.add_handler(CommandHandler(command='roll',
+                                              filters=Filters.all,
+                                              callback=roll))
 
 def _get_transactions():
     transactions = []
     get_chain_address = f"{spc_url}/transactions/{public_key}"
-    print('get chain address', get_chain_address)
     response = requests.get(get_chain_address)
     if response.status_code == 200:
         data = json.loads(response.content)
-        print('data', data['transactions'])
+        return sorted(data['transactions'],
+                      key=lambda k: k['timestamp'],
+                      reverse=True)
+    return []
+
+def _get_default_timestamp():
+    arw = arrow.utcnow()
+    arw = arw.shift(days=-1)
+    return arw.datetime
+
+def _get_address_timestamps():
+    values = {}
+    for tx in Transaction.select():
+        values[tx.address] = tx.timestamp
+    return values
+
+def _update_timestamp(address, timestamp):
+    Transaction\
+        .insert(address=address, timestamp=timestamp)\
+        .on_conflict('replace')\
+        .execute()
 
 
 def job_callback(st):
-    print('job callback', st)
-    _get_transactions()
+    timestamps = _get_address_timestamps()
+    for tx in _get_transactions():
+        try:
+            address = Address.get(Address.address==tx['from_address'])
+            timestamp = timestamps.get(address.address, _get_default_timestamp())
+            tx_timestamp = arrow.get(tx['timestamp'])
+            if timestamp > tx_timestamp:
+                continue
+            else:
+                amount = tx['amount']
+                new_rolls = int(amount / ROLL_PRICE)
+                old_rolls = _get_roll_count(address.user_id)
+                print('new rolls', new_rolls, 'old rolls', old_rolls)
+                _set_roll_count(address.user_id, new_rolls + old_rolls)
+        except Exception as e:
+            pass
 
 jobs = JobQueue()
 jobs.set_dispatcher(updater.dispatcher)
