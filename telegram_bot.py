7237731273ff08e91931740d358f44617731ca40
AJ Nieminen <ajnieminen@kapsi.fi>
2020-11-22 16:59:08 +0200

    telegram bot changes

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,6 +5,7 @@ from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler
 from telegram.ext import Filters
 from telegram.ext import CallbackContext
+from telegram.ext import JobQueue
 
 from peewee import SqliteDatabase
 from peewee import Model
@@ -15,12 +16,42 @@ from peewee import IntegerField
 
 from datetime import datetime as dt
 
+from nacl.public import PrivateKey
+from nacl.signing import SigningKey
+from nacl.encoding import HexEncoder
+from nacl.encoding import Base64Encoder
+from nacl.encoding import URLSafeBase64Encoder
+from nacl.bindings.crypto_sign import crypto_sign_ed25519_sk_to_pk
+from nacl.bindings.crypto_sign import crypto_sign_keypair
+
+import requests
+import json
+import nacl.secret
+import nacl.utils
+
+
 from os import environ
 
 # pip install python-telegram-bot
 # pip install peewee
 
 db = SqliteDatabase('spc.db')
+
+class Roll(Model):
+    user_id = IntegerField()
+    count = IntegerField()
+
+    class Meta:
+        database = db
+
+class Transaction(Model):
+    to_address = CharField()
+    from_address = CharField()
+    amount = IntegerField()
+    timestamp = DateTimeField()
+
+    class Meta:
+        database = db
 
 class Address(Model):
     user_id = IntegerField()
@@ -36,12 +67,18 @@ class Address(Model):
 
 def _create_tables():
     with db:
-        db.create_tables([Address])
+        db.create_tables([Roll, Transaction, Address])
+
+def _get_keys(secret_key):
+    public_key = crypto_sign_ed25519_sk_to_pk(bytes.fromhex(secret_key))
+    return secret_key, public_key.hex()
 
 _create_tables()
 token = environ['tbot_api']
 spc_url = environ['spc_url']
 spc_wallet = environ['spc_wallet']
+
+secret_key, public_key = _get_keys(spc_wallet)
 
 updater = Updater(token, use_context=True)
 
@@ -72,6 +109,25 @@ def message(update, context):
 updater.dispatcher.add_handler(CommandHandler(command='osoite',
                                               filters=Filters.all,
                                               callback=address))
+
+def _get_transactions():
+    transactions = []
+    get_chain_address = f"{spc_url}/transactions/{public_key}"
+    print('get chain address', get_chain_address)
+    response = requests.get(get_chain_address)
+    if response.status_code == 200:
+        data = json.loads(response.content)
+        print('data', data['transactions'])
+
+
+def job_callback(st):
+    print('job callback', st)
+    _get_transactions()
+
+jobs = JobQueue()
+jobs.set_dispatcher(updater.dispatcher)
+jobs.run_repeating(callback=job_callback, interval=10)
+jobs.start()
 
 updater.start_polling()
 updater.idle()
