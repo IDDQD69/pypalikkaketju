4eede9756648f8eca90ac10364b5622e4505d38a
AJ Nieminen <ajnieminen@kapsi.fi>
2020-11-27 17:09:26 +0200

    mps

--- run_app.py
+++ run_app.py
@@ -1,14 +1,14 @@
 from app import app
 from os import getenv
 
-hostname = getenv('SPC_HOSTNAME', 'http://localhost/')
+hostname = getenv('SPC_HOSTNAME', 'localhost')
 port = getenv('SPC_PORT', 5000)
 debug = getenv('SPC_DEBUG', False)
 application_root = getenv('SPC_APPLICATION_ROOT', '')
 
 app.run(debug=debug,
-        hostname=hostname,
+        host=hostname,
         port=port)
 
 if application_root and len(application_root):
-    app.config["APPLICATION_ROOT"] = "/spc"
+    app.config["APPLICATION_ROOT"] = application_root
