3abca26b17fad71680b851f8a34d9976d25f80fb
AJ Nieminen <ajnieminen@kapsi.fi>
2020-11-27 17:00:12 +0200

    mps

--- run_app.py
+++ run_app.py
@@ -1,4 +1,14 @@
 from app import app
+from os import getenv
 
-app.run(debug=True,
-        ssl_context=('server.crt', 'server.key'))
+hostname = getenv('SPC_HOSTNAME', 'http://localhost/')
+port = getenv('SPC_PORT', 5000)
+debug = getenv('SPC_DEBUG', False)
+application_root = getenv('SPC_APPLICATION_ROOT', '')
+
+app.run(debug=debug,
+        hostname=hostname,
+        port=port)
+
+if application_root and len(application_root):
+    app.config["APPLICATION_ROOT"] = "/spc"
