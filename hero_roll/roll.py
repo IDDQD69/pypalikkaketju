49de9a0f3f49f7dbcf870a209b9d3ce9b3959978
AJ Nieminen <ajnieminen@kapsi.fi>
2021-01-21 22:22:24 +0200

    changes

--- hero_roll/roll.py
+++ hero_roll/roll.py
@@ -6,10 +6,13 @@ from .models import Hero
 
 from . import get_settings
 
-def _get_next_xp_requirement(hero: Hero):
-    pass
 
-def _get_hero_xp_requirement(hero: Hero, settings: dict):
+def _get_roll_xp(settings: dict, win_value: int) -> int:
+    return settings['settings']['roll_xp_win']\
+        if win_value > 0 \
+           else settings['settings']['roll_xp']
+
+def _get_hero_xp_requirement(hero: Hero, settings: dict) -> int:
     return settings['settings']['base_xp']\
         + (hero.level * settings['settings']['next_xp'])
 
@@ -25,9 +28,15 @@ def _get_hero(user_id) -> Hero:
 
 
 def handle_hero_roll(update: Update, win_value: int):
+    settings = get_settings()
     user: User = update.effective_user
     hero: Hero = _get_hero(user.id)
 
-    settings = get_settings()
+    print('hero xp')
+    hero.xp = hero.xp + _get_roll_xp(settings, win_value)
+
     xp_req = _get_hero_xp_requirement(hero, settings)
-    print('xp re', xp_req)
+    if hero.xp >= xp_req:
+        hero.level = hero.level + 1
+    hero.save()
+    print('xp re', xp_req, hero.xp)
