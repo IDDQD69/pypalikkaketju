52d1f0e4bfa9f7ad0ed406e4323ef07ece711842
AJ Nieminen <ajnieminen@kapsi.fi>
2021-01-21 21:51:25 +0200

    changes to heroroll

--- hero_roll/roll.py
+++ hero_roll/roll.py
@@ -6,10 +6,12 @@ from .models import Hero
 
 from . import get_settings
 
-
 def _get_next_xp_requirement(hero: Hero):
     pass
 
+def _get_hero_xp_requirement(hero: Hero, settings: dict):
+    return settings['settings']['base_xp']\
+        + (hero.level * settings['settings']['next_xp'])
 
 def _get_hero(user_id) -> Hero:
     try:
@@ -18,7 +20,7 @@ def _get_hero(user_id) -> Hero:
         return Hero.create(
             user_id=user_id,
             name='',
-            level=1,
+            level=0,
             xp=0)
 
 
@@ -26,4 +28,6 @@ def handle_hero_roll(update: Update, win_value: int):
     user: User = update.effective_user
     hero: Hero = _get_hero(user.id)
 
-    print('handle_hero_roll', user, hero, get_settings())
+    settings = get_settings()
+    xp_req = _get_hero_xp_requirement(hero, settings)
+    print('xp re', xp_req)
