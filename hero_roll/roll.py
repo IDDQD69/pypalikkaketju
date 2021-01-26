d359bb9e6deed5f34915fad1a7bd766fc923daa8
ajnieminen <ajnieminen@kapsi.fi>
2021-01-26 20:02:38 +0200

    changes

--- hero_roll/roll.py
+++ hero_roll/roll.py
@@ -8,9 +8,7 @@ from . import get_settings
 
 
 def _get_roll_xp(settings: dict, win_value: int) -> int:
-    # TODO: xp + win_value / 100?
-    # possible to get multiple levels at once
-    return settings['settings']['roll_xp_win']\
+    return win_value * settings['settings']['roll_xp_win']\
         if win_value > 0 \
            else settings['settings']['roll_xp']
 
@@ -31,16 +29,25 @@ def _get_hero(user_id) -> Hero:
             xp=0)
 
 
+def _handle_hero_level(settings, hero):
+    fail_counter = 0
+    xp_req = _get_hero_xp_requirement(hero, settings)
+    while hero.xp >= xp_req:
+        hero.level = hero.level + 1
+        xp_req = _get_hero_xp_requirement(hero, settings)
+        fail_counter = fail_counter + 1
+        if fail_counter < 300:
+            break
+
+
 def handle_hero_roll(update: Update, win_value: int):
     settings = get_settings()
     user: User = update.effective_user
     hero: Hero = _get_hero(user.id)
 
-    print('hero xp')
     hero.xp = hero.xp + _get_roll_xp(settings, win_value)
 
-    xp_req = _get_hero_xp_requirement(hero, settings)
-    if hero.xp >= xp_req:
-        hero.level = hero.level + 1
+    _handle_hero_level(settings, hero)
     hero.save()
-    print('xp re', xp_req, hero.xp)
+    xp_req = _get_hero_xp_requirement(hero, settings)
+    print('xp re', xp_req, hero.xp, hero.level)
